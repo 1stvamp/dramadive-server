8ea73529cbb502e3367a6f14deb2f2dbe91f37bd
Wes Mason <wes@1stvamp.org>
2014-08-02 22:26:14 +0100

    Add trequests and example yaml config

--- dramadive/server.py
+++ dramadive/server.py
@@ -4,11 +4,20 @@ from sys import stdout
 from tornado.ioloop import IOLoop
 from tornado.web import RequestHandler, Application
 
+from trequests import setup_session
+from tornalet import tornalet
+
+# Tell requests to use our AsyncHTTPadapter for the default
+# session instance
+setup_session()
+
+
 
 class MainHandler(RequestHandler):
     def prepare(self):
         self.set_header('Content-Type', 'application/json; charset="utf-8"')
 
+    @tornalet
     def post(self):
         action = self.get_argument('action', '')
         self.write({'action': action})
