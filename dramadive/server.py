da99d9f9e9c5670659edb551428fa412fcaff03a
Wes Mason <wes@1stvamp.org>
2014-08-03 01:53:27 +0100

    Add paypal CC payments

--- dramadive/server.py
+++ dramadive/server.py
@@ -1,20 +1,50 @@
 #!/usr/bin/env python3
 
+from os.path import dirname, join
 from sys import stdout
-from os.path import join, dirname
 from time import time
 
+import paypalrestsdk
+from pprint import pprint
+from tornado.ioloop import IOLoop, PeriodicCallback
+from tornado.web import Application, RequestHandler
 from yaml import load
 
-from tornado.ioloop import IOLoop, PeriodicCallback
-from tornado.web import RequestHandler, Application
 
-from trequests import setup_session
-from tornalet import tornalet
+def paypal_transfer(config):
+    payment = paypalrestsdk.Payment({
+        "intent": "sale",
+        "payer": {
+            "payment_method": "credit_card",
+            "funding_instruments": [{
+                "credit_card": {
+                    "type": "visa",
+                    "number": config['paypal']['visa_account_no'],
+                    "expire_month": config['paypal']['visa_exp_month'],
+                    "expire_year": config['paypal']['visa_exp_year'],
+                    "cvv2": "874",
+                    "first_name": "Joe",
+                    "last_name": "Shopper"
+                }}]},
+            "transactions": [{
+                "item_list": {
+                    "items": [{
+                        "name": "item",
+                        "sku": "item",
+                        "price": "{0:.2f}".format(config['giving']),
+                        "currency": "USD",
+                        "quantity": 1
+                        }]},
+                    "amount": {
+                        "total": "{0:.2f}".format(config['giving']),
+                        "currency": "USD"
+                        },
+                    "description": 'dramadive donation'}]})
 
-# Tell requests to use our AsyncHTTPadapter for the default
-# session instance
-setup_session()
+    if payment.create():
+        print("Payment created successfully")
+    else:
+        print(payment.error)
 
 
 def climber_mode(config, action=False):
@@ -22,7 +52,10 @@ def climber_mode(config, action=False):
         config['giving'] = 0.01
     else:
         config['giving'] += 0.10
-    print(config['giving'])
+
+        paypal_transfer(config)
+
+    print("{0:.2f}".format(config['giving']))
 
 
 def generous_mode(config, action=False):
@@ -33,7 +66,10 @@ def generous_mode(config, action=False):
             config['giving'] -= 0.10
         else:
             config['giving'] = 0.01
-    print(config['giving'])
+
+        paypal_transfer(config)
+
+    print("{0:.2f}".format(config['giving']))
 
 
 MODE_MAP = {
@@ -50,7 +86,6 @@ class MainHandler(RequestHandler):
     def prepare(self):
         self.set_header('Content-Type', 'application/json; charset="utf-8"')
 
-    @tornalet
     def post(self):
         action = self.get_argument('action', '')
         self.write({'action': action})
@@ -66,6 +101,12 @@ if __name__ == "__main__":
     config = load(open(join(dirname(__file__), '../config.yaml'), 'r'))
     config['giving'] = 0.01
     application.dd_config = config
+
+    paypalrestsdk.configure({
+        "mode": "sandbox",
+        "client_id": config['paypal']['api']['client_id'],
+        "client_secret": config['paypal']['api']['client_secret']
+    })
 
     application.listen(8888)
     print('Service started on http://localhost:8888/', file=stdout)
